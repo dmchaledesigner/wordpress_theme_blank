cfe18d30142ddf1a79ed4b49ceba99cbc9e7a3fa
dmchaledesigner <dmchaledesigner@gmail.com>
2016-02-15 15:29:19 +0800

    unregister default widgets in functions file

--- wp-content/themes/starkers/functions.php
+++ wp-content/themes/starkers/functions.php
@@ -287,6 +287,35 @@ endif;
 
 
 
+
+
+/*---------------------
+ Unregister default widgets.
+ ------------------------*/
+
+function custom_unregister_default_widgets() {
+
+    unregister_widget( 'WP_Widget_Archives' );
+    unregister_widget( 'WP_Widget_Calendar' );
+    unregister_widget( 'WP_Widget_Categories' );
+    unregister_widget( 'WP_Widget_Meta' );
+    unregister_widget( 'WP_Widget_Pages' );
+    unregister_widget( 'WP_Widget_Recent_Comments' );
+    unregister_widget( 'WP_Widget_Recent_Posts' );
+    unregister_widget( 'WP_Widget_RSS' );
+    unregister_widget( 'WP_Widget_Search' );
+    unregister_widget( 'WP_Nav_Menu_Widget' );
+    unregister_widget( 'WP_Widget_Tag_Cloud' );
+    unregister_widget( 'Akismet_Widget' );
+
+}
+add_action( 'widgets_init', 'custom_unregister_default_widgets' );
+
+
+
+
+
+
 // Registering CSS Files
 
     function myStyles()
