117f86edeb79f9d354c4d68ec75ebd4f0ffd60df
dmchaledesigner <dmchaledesigner@gmail.com>
2016-02-17 08:25:35 +0800

    added home page template

---
+++ wp-content/themes/starkers/home.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Template Name: home
+ *
+ * @package WordPress
+ * @subpackage Starkers
+ * @since Starkers HTML5 3.0
+ */
+
+get_header(); ?>
+
+	
+
+
+
+
+	
+<?php get_footer(); ?>
